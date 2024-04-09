8e8a9dd43a225b59fa5a1c47ba1d843b7a0828fc
Joe-Mar Gonzales <rameoj18@gmail.com>
2024-04-09 09:09:27 +0800

    added some docs

--- src/PseRestApi.Core/Services/PseApi/IPseClient.cs
+++ src/PseRestApi.Core/Services/PseApi/IPseClient.cs
@@ -4,6 +4,10 @@ namespace PseRestApi.Core.Services.PseApi;
 
 public interface IPseClient
 {
+    /// <summary>
+    /// Get current stocks data from PSE
+    /// </summary>
+    /// <returns>List of all stocks with current prices</returns>
     Task<IEnumerable<StockFromFrames>> GetStocks();
     Task<StockCompanyResponse> FindSecurityOrCompany(string symbol);
 
